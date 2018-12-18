c40c7d22c4373ddd4de0eaa65b4feae2f02d123e
zhangfuli <1171067930@qq.com>
2018-12-18 10:29:26 +0800

    react前端基本框架

---
+++ dop-web/src/App.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+import './App.css';
+import router from './router/router'
+
+class App extends Component {
+  render() {
+    return router;
+  }
+}
+
+export default App;
