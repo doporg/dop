ff5bf261dde1ab5d98aedf1e828a5a14c322a898
任贵杰 <812022339@qq.com>
2019-02-24 02:53:27 +0800

    API网关聚合文档

--- gateway-server/src/main/java/com/clsaa/dop/server/gateway/config/SwaggerConfig.java
+++ gateway-server/src/main/java/com/clsaa/dop/server/gateway/config/SwaggerConfig.java
@@ -1,36 +1,46 @@
 package com.clsaa.dop.server.gateway.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * swagger配置类
  *
  * @author 任贵杰 812022339@qq.com
  */
+@Primary
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig implements SwaggerResourcesProvider {
 
     private static final String API_PACKAGE_NAME = SwaggerConfig.class.getPackage().getName().replace("config", "controller");
-
-    @Value("project.groupId")
+    @Autowired
+    RouteLocator routeLocator;
+    @Value("${project.groupId}")
     private String groupId;
-    @Value("project.artifactId")
+    @Value("${project.artifactId}")
     private String artifactId;
-    @Value("project.version")
+    @Value("${project.version}")
     private String version;
-    @Value("project.name")
+    @Value("${project.name}")
     private String name;
-    @Value("project.description")
+    @Value("${project.description}")
     private String description;
-    @Value("project.url")
+    @Value("${project.url}")
     private String url;
 
     @Bean
@@ -50,5 +60,26 @@ public class SwaggerConfig {
                 .termsOfServiceUrl(url)
                 .version(version)
                 .build();
+    }
+
+    @Override
+    public List<SwaggerResource> get() {
+        //利用routeLocator动态引入微服务
+        List<SwaggerResource> resources = new ArrayList<>();
+        resources.add(swaggerResource("gateway-server","/v2/api-docs","1.0"));
+        //循环 使用Lambda表达式简化代码
+        routeLocator.getRoutes().forEach(route -> {
+            //动态获取
+            resources.add(swaggerResource(route.getId(), route.getFullPath().replace("**", "v2/api-docs"), "1.0"));
+        });
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location, String version) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion(version);
+        return swaggerResource;
     }
 }
