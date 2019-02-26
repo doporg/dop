31e461b620dda9696c87d872dc6cfb8e735f7007
任贵杰 <812022339@qq.com>
2019-02-26 22:29:03 +0800

    编码规范

--- user-server/src/main/java/com/clsaa/dop/server/user/controller/UserController.java
+++ user-server/src/main/java/com/clsaa/dop/server/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.clsaa.dop.server.user.controller;
 
+import com.alibaba.druid.sql.visitor.functions.If;
 import com.clsaa.dop.server.user.config.BizCodes;
 import com.clsaa.dop.server.user.model.dto.ResetDtoV1;
 import com.clsaa.dop.server.user.model.vo.UserV1;
@@ -7,8 +8,10 @@ import com.clsaa.dop.server.user.util.BeanUtils;
 import com.clsaa.dop.server.user.service.UserService;
 import com.clsaa.rest.result.Pagination;
 import com.clsaa.rest.result.bizassert.BizAssert;
+import com.clsaa.rest.result.bizassert.BizCode;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,10 +35,22 @@ public class UserController {
         this.userService.addUser(code);
     }
 
-    @GetMapping("/test")
-    public boolean test() {
-        BizAssert.justDenied(BizCodes.ERROR_DELETE);
+    @GetMapping("/test/{id}")
+    public boolean test(@PathVariable("id") String id) {
+        BizAssert.validParam(StringUtils.isNotBlank(id) && id.length() == 6,
+                new BizCode(BizCodes.INVALID_PARAM.getCode(), "参数id非法"));
+        try {
+            Integer intId = Integer.valueOf(id);
+        } catch (Exception e) {
+            BizAssert.justInvalidParam(BizCodes.INVALID_PARAM);
+        }
+        int changedCount = this.deleteUser(id);
+        BizAssert.found(changedCount == 1, BizCodes.ERROR_DELETE);
         return true;
+    }
+
+    private int deleteUser(String id) {
+        return id.equals("123456") ? 1 : 0;
     }
 
     @ApiOperation(value = "更新用户密码", notes = "用户必须先获取修改密码所需验证码，通过验证码更新密码")
