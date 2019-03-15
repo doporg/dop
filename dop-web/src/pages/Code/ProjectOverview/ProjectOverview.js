59128a6a6e7b4f4864d37a5e9f99041c15806a08
waszqt <32014294+waszqt@users.noreply.github.com>
2019-03-15 20:05:25 +0800

    项目总览ui

--- dop-web/src/pages/Code/ProjectOverview/ProjectOverview.js
+++ dop-web/src/pages/Code/ProjectOverview/ProjectOverview.js
@@ -1,22 +1,71 @@
 import React from 'react';
 import IceLabel from '@icedesign/label';
-import {Button} from '@icedesign/base';
-
+import FoundationSymbol from 'foundation-symbol';
 import './ProjectOverview.css'
 
 import imgPublic from './imgs/public.png'
-
+import imgStar from './imgs/star.png'
+import imgBranch from './imgs/branch.png'
+import imgDownload from './imgs/download.png'
 
 export default class ProjectOverview extends React.Component{
+
+    componentWillMount(){
+
+    }
 
     render(){
 
         return (
 
             <div className="container">
-                <Button type="normal" className="btn btn_public">
+                <button className="btn btn_public">
                     <img src={imgPublic}/>PUBLIC
-                </Button>
+                </button>
+                <button className="btn btn_edit">
+                    <FoundationSymbol size="small" type="edit2" >
+                    </FoundationSymbol>
+                </button>
+                <div className="project_avatar">
+                    H
+                </div>
+
+                <div>
+                    <p className="text_title">hhh</p>
+                    <p className="text_description">This is a description!</p>
+                </div>
+
+                <div className="div1">
+                    <button className="btn btn_star">
+                        <img src={imgStar}/>0
+                    </button>
+                    <button className="btn btn_branch">
+                        <img src={imgBranch}/>0
+                    </button>
+
+                    <button className="btn btn_ssh">
+                        SSH
+                    </button>
+                    <button className="btn btn_http">
+                        HTTP
+                    </button>
+                    <input className="input_url" type="text"/>
+                    <button className="btn btn_copy">
+                        <FoundationSymbol size="small" type="copy" >
+                        </FoundationSymbol>
+                    </button>
+                    <button className="btn btn_download">
+                        <img src={imgDownload}/>
+                    </button>
+                </div>
+
+                <div className="div2">
+                    <span>1次提交</span>
+                    <span>1个分支</span>
+                    <span>0个标签</span>
+                    <span>0.14MB</span>
+                </div>
+
             </div>
         );
     }
