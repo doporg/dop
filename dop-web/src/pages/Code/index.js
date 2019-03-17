c348496efa032ca55015ad664c44b059bc1a304f
waszqt <32014294+waszqt@users.noreply.github.com>
2019-03-17 13:24:47 +0800

    rename Project

--- dop-web/src/pages/Code/index.js
+++ dop-web/src/pages/Code/index.js
@@ -1,4 +1,4 @@
 import Code from './DataOverview'
-import {PersonalProjects,StarredProjects,AllProjects} from './projects'
+import {PersonalProjects,StarredProjects,AllProjects} from './Projects'
 import ProjectOverview from './ProjectOverview'
 export {Code,PersonalProjects,StarredProjects,AllProjects,ProjectOverview};
