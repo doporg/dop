72532f60205b9456b3e6d05140d057aa6e4f64af
151250010 <851642452@qq.com>
2019-03-22 10:13:30 +0800

    commit to checkout

--- test-server/src/main/java/com/clsaa/dop/server/test/doExecute/TestManager.java
+++ test-server/src/main/java/com/clsaa/dop/server/test/doExecute/TestManager.java
@@ -1,6 +1,11 @@
 package com.clsaa.dop.server.test.doExecute;
 
 import com.clsaa.dop.server.test.model.dto.InterfaceCaseDto;
+import com.clsaa.dop.server.test.model.dto.InterfaceStageDto;
+
+import java.util.List;
+
+import static java.util.Objects.isNull;
 
 /**
  * 完成测试脚本的执行
@@ -10,8 +15,17 @@ import com.clsaa.dop.server.test.model.dto.InterfaceCaseDto;
  */
 public class TestManager {
 
+    private static final String FAIL_RESULT = "fail";
+    private static final String SUCCESS_RESULT = "success";
+
     public static String execute(InterfaceCaseDto interfaceCaseDto) {
-        // todo logic of executing test cases
-        return "fail";
+        if (isNull(interfaceCaseDto)) {
+            //todo define Result Info
+            return FAIL_RESULT;
+        }
+
+        List<InterfaceStageDto> stages = interfaceCaseDto.getStages();
+
+        return FAIL_RESULT;
     }
 }
