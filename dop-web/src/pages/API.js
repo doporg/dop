7e754b524d577bbf3bea6f33013290ddde387500
任贵杰 <812022339@qq.com>
2019-03-25 19:05:17 +0800

    修改API地址

--- dop-web/src/pages/API.js
+++ dop-web/src/pages/API.js
@@ -1,14 +1,14 @@
 // 配置所有接口的API文件
 
 const API = {
-    gateway: "http://localhost:8888",
-    pipeline: "http://localhost:13600",
+    gateway: "http://open.dop.clsaa.com",
+    pipeline: "http://open.dop.clsaa.com/pipeline-server",
     jenkins: "http://jenkins.dop.clsaa.com",
     jenkinsRest: "http://jenkins.dop.clsaa.com/blue/rest/organizations/jenkins/pipelines/",
     address: "http://www.dop.clsaa.com/#/",
-    application: "http://localhost:13800",
-    permission: "http://localhost:13300",
-    code:"http://localhost:13900"
+    application: "http://open.dop.clsaa.com/application-server",
+    permission: "http://open.dop.clsaa.com/permission-server",
+    code:"http://open.dop.clsaa.com/code-server"
 };
 
 export default API;
