910e4072c3c4f5af64ba31805263a3f4e4314850
ZhiTingXin <151250170@smail.nju.edu.cn>
2019-04-16 15:01:55 +0800

    创建日志界面

--- dop-web/src/pages/Image/index.js
+++ dop-web/src/pages/Image/index.js
@@ -2,6 +2,7 @@ import Repos from './ReposManagement/Repos';
 import BasicLayout from '../../layouts/BasicLayout';
 import Image from './Image';
 import ImageList from "./ImageList/ImageList";
+import NamespaceLogList from "./NamespaceLog/NamespaceLogList";
 
 const imageConfig =[
     {
@@ -18,7 +19,12 @@ const imageConfig =[
         path: '/image/projects/:projectId/repos/:projectName/:repoName/images',
         layout: BasicLayout,
         component: ImageList
+    },
+    {
+        path: '/image/projects/:projectId/logs',
+        layout: BasicLayout,
+        component: NamespaceLogList
     }
 ];
 
-export {Repos,imageConfig,Image,ImageList}
+export {Repos,imageConfig,Image,ImageList,NamespaceLogList}
