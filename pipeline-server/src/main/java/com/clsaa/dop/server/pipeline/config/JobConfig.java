4dd5d917ca388433a9115eeb4aec4bdc3c3ff9c6
zhangfuli <1171067930@qq.com>
2019-05-01 22:22:20 +0800

    定时触发-bug

--- pipeline-server/src/main/java/com/clsaa/dop/server/pipeline/config/JobConfig.java
+++ pipeline-server/src/main/java/com/clsaa/dop/server/pipeline/config/JobConfig.java
@@ -10,47 +10,12 @@ package com.clsaa.dop.server.pipeline.config;
 public class JobConfig {
     private String script;
     private String xml;
-    private String timing;
-
-    public JobConfig() {
-        this.script = "";
-        this.xml = "<flow-definition plugin=\"workflow-job@2.31\">\n" +
-                "  <actions>\n" +
-                "    <org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobAction plugin=\"pipeline-model-definition@1.3.4.1\"/>\n" +
-                "    <org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobPropertyTrackerAction plugin=\"pipeline-model-definition@1.3.4.1\">\n" +
-                "      <jobProperties/>\n" +
-                "      <triggers/>\n" +
-                "      <parameters/>\n" +
-                "      <options/>\n" +
-                "    </org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobPropertyTrackerAction>\n" +
-                "  </actions>\n" +
-                "  <description></description>\n" +
-                "  <keepDependencies>false</keepDependencies>\n" +
-                "  <properties/>\n" +
-                "  <definition class=\"org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition\" plugin=\"workflow-cps@2.61.1\">\n" +
-                "    <script>" +
-
-                "    </script>\n" +
-                "    <sandbox>true</sandbox>\n" +
-                "  </definition>\n" +
-                "  <triggers/>\n" +
-                "  <disabled>false</disabled>\n" +
-                "</flow-definition>";
-    }
-
     public JobConfig(String script, String timing) {
         this.script = script;
-        this.timing = timing;
         this.xml = "<flow-definition plugin=\"workflow-job@2.31\">\n" +
                 "  <actions>\n" +
-                "    <org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobAction plugin=\"pipeline-model-definition@1.3.4.1\"/>\n" +
-                "    <org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobPropertyTrackerAction plugin=\"pipeline-model-definition@1.3.4.1\">\n" +
-                "      <jobProperties/>\n" +
-                "      <triggers/>\n" +
-                "      <parameters/>\n" +
-                "      <options/>\n" +
-                "    </org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobPropertyTrackerAction>\n" +
-                "  </actions>\n" +
+                "\t\t<org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobAction plugin=\"pipeline-model-definition@1.3.4\"/>\n" +
+                "\t</actions>\n" +
                 "  <description></description>\n" +
                 "  <keepDependencies>false</keepDependencies>\n" +
                 "  <properties>\n" +
@@ -67,7 +32,7 @@ public class JobConfig {
                 "\t\t<org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty>\n" +
                 "\t\t\t<triggers>\n" +
                 "\t\t\t\t<hudson.triggers.TimerTrigger>\n" +
-                "\t\t\t\t\t<spec>"+ this.timing +"</spec>\n" +
+                "\t\t\t\t\t<spec>"+ timing +"</spec>\n" +
                 "\t\t\t\t</hudson.triggers.TimerTrigger>\n" +
                 "\t\t\t</triggers>\n" +
                 "\t\t</org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty>\n" +
@@ -84,53 +49,55 @@ public class JobConfig {
     }
 
     public JobConfig(String git, String path, String timing) {
-        this.xml = "<flow-definition plugin=\"workflow-job@2.31\">\n" +
-                "<actions/>\n" +
-                "<description/>\n" +
-                "<keepDependencies>false</keepDependencies>\n" +
-                "<properties>\n" +
-                "<com.dabsquared.gitlabjenkins.connection.GitLabConnectionProperty plugin=\"gitlab-plugin@1.5.12\">\n" +
-                "<gitLabConnection/>\n" +
-                "</com.dabsquared.gitlabjenkins.connection.GitLabConnectionProperty>\n" +
-                "<org.jenkinsci.plugins.gitlablogo.GitlabLogoProperty plugin=\"gitlab-logo@1.0.3\">\n" +
-                "<repositoryName/>\n" +
-                "</org.jenkinsci.plugins.gitlablogo.GitlabLogoProperty>\n" +
-                "<com.sonyericsson.rebuild.RebuildSettings plugin=\"rebuild@1.29\">\n" +
-                "<autoRebuild>false</autoRebuild>\n" +
-                "<rebuildDisabled>false</rebuildDisabled>\n" +
-                "</com.sonyericsson.rebuild.RebuildSettings>\n" +
-                "<org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty>\n" +
-                "<triggers>\n" +
-                "<hudson.triggers.TimerTrigger>\n" +
-                "<spec>"+ timing +"</spec>\n" +
-                "</hudson.triggers.TimerTrigger>\n" +
-                "</triggers>\n" +
-                "</org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty>\n" +
-                "</properties>\n" +
-                "<definition class=\"org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition\" plugin=\"workflow-cps@2.61\">\n" +
-                "<scm class=\"hudson.plugins.git.GitSCM\" plugin=\"git@3.9.1\">\n" +
-                "<configVersion>2</configVersion>\n" +
-                "<userRemoteConfigs>\n" +
-                "<hudson.plugins.git.UserRemoteConfig>\n" +
-                "<url>\n" +
-                git +"\n" +
-                "</url>\n" +
-                "</hudson.plugins.git.UserRemoteConfig>\n" +
-                "</userRemoteConfigs>\n" +
-                "<branches>\n" +
-                "<hudson.plugins.git.BranchSpec>\n" +
-                "<name>*/master</name>\n" +
-                "</hudson.plugins.git.BranchSpec>\n" +
-                "</branches>\n" +
-                "<doGenerateSubmoduleConfigurations>false</doGenerateSubmoduleConfigurations>\n" +
-                "<submoduleCfg class=\"list\"/>\n" +
-                "<extensions/>\n" +
-                "</scm>\n" +
-                "<scriptPath>"+path+"</scriptPath>\n" +
-                "<lightweight>true</lightweight>\n" +
-                "</definition>\n" +
-                "<triggers/>\n" +
-                "<disabled>false</disabled>\n" +
+        this.xml = "<?xml version='1.1' encoding='UTF-8'?>\n"
+                +"<flow-definition plugin=\"workflow-job@2.31\">\n" +
+                "\t<actions>\n" +
+                "\t\t<org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobAction plugin=\"pipeline-model-definition@1.3.4\"/>\n" +
+                "\t</actions>\n" +
+                "\t<description/>\n" +
+                "\t<keepDependencies>false</keepDependencies>\n" +
+                "\t<properties>\n" +
+                "\t\t<org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty>\n" +
+                "\t\t\t<triggers>\n" +
+                "\t\t\t\t<hudson.triggers.TimerTrigger>\n" +
+                "\t\t\t\t\t<spec>"+timing+"</spec>\n" +
+                "\t\t\t\t</hudson.triggers.TimerTrigger>\n" +
+                "\t\t\t</triggers>\n" +
+                "\t\t</org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty>\n" +
+                "\t\t<com.dabsquared.gitlabjenkins.connection.GitLabConnectionProperty plugin=\"gitlab-plugin@1.5.12\">\n" +
+                "\t\t\t<gitLabConnection/>\n" +
+                "\t\t</com.dabsquared.gitlabjenkins.connection.GitLabConnectionProperty>\n" +
+                "\t\t<org.jenkinsci.plugins.gitlablogo.GitlabLogoProperty plugin=\"gitlab-logo@1.0.3\">\n" +
+                "\t\t\t<repositoryName/>\n" +
+                "\t\t</org.jenkinsci.plugins.gitlablogo.GitlabLogoProperty>\n" +
+                "\t\t<com.sonyericsson.rebuild.RebuildSettings plugin=\"rebuild@1.29\">\n" +
+                "\t\t\t<autoRebuild>false</autoRebuild>\n" +
+                "\t\t\t<rebuildDisabled>false</rebuildDisabled>\n" +
+                "\t\t</com.sonyericsson.rebuild.RebuildSettings>\n" +
+                "\t</properties>\n" +
+                "\t<definition class=\"org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition\" plugin=\"workflow-cps@2.61\">\n" +
+                "\t\t<scm class=\"hudson.plugins.git.GitSCM\" plugin=\"git@3.9.1\">\n" +
+                "\t\t\t<configVersion>2</configVersion>\n" +
+                "\t\t\t<userRemoteConfigs>\n" +
+                "\t\t\t\t<hudson.plugins.git.UserRemoteConfig>\n" +
+                "\t\t\t\t\t<url>" +
+                git.trim()+"</url>\n" +
+                "\t\t\t\t</hudson.plugins.git.UserRemoteConfig>\n" +
+                "\t\t\t</userRemoteConfigs>\n" +
+                "\t\t\t<branches>\n" +
+                "\t\t\t\t<hudson.plugins.git.BranchSpec>\n" +
+                "\t\t\t\t\t<name>*/master</name>\n" +
+                "\t\t\t\t</hudson.plugins.git.BranchSpec>\n" +
+                "\t\t\t</branches>\n" +
+                "\t\t\t<doGenerateSubmoduleConfigurations>false</doGenerateSubmoduleConfigurations>\n" +
+                "\t\t\t<submoduleCfg class=\"list\"/>\n" +
+                "\t\t\t<extensions/>\n" +
+                "\t\t</scm>\n" +
+                "\t\t<scriptPath>"+path.trim()+"</scriptPath>\n" +
+                "\t\t<lightweight>true</lightweight>\n" +
+                "\t</definition>\n" +
+                "\t<triggers/>\n" +
+                "\t<disabled>false</disabled>\n" +
                 "</flow-definition>";
     }
 
