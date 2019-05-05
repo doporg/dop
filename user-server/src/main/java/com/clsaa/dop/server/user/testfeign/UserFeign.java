503389be30346d2d4c21fe72c42aef02da9c9fc5
任贵杰 <812022339@qq.com>
2019-05-05 13:06:02 +0800

    image-server

--- user-server/src/main/java/com/clsaa/dop/server/user/testfeign/UserFeign.java
+++ user-server/src/main/java/com/clsaa/dop/server/user/testfeign/UserFeign.java
@@ -1,6 +1,8 @@
 package com.clsaa.dop.server.user.testfeign;
 
 import com.clsaa.dop.server.user.config.FeignConfig;
+import com.clsaa.dop.server.user.model.po.UserCredential;
+import com.clsaa.dop.server.user.model.vo.UserCredentialV1;
 import com.clsaa.dop.server.user.model.vo.UserV1;
 import com.clsaa.rest.result.Pagination;
 import io.swagger.annotations.ApiOperation;
@@ -8,6 +10,7 @@ import io.swagger.annotations.ApiParam;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -26,5 +29,7 @@ public interface UserFeign {
                                                                     @RequestParam(value = "pageNo", required = false, defaultValue = "1") Integer pageNo,
                                                                     @ApiParam(value = "页大小")
                                                                     @RequestParam(value = "pageSize", required = false, defaultValue = "10") Integer pageSize);
-
+    @GetMapping(value = "/v1/users/{userId}/credential")
+    UserCredentialV1 getUserCredentialV1ByUserId(@PathVariable("userId") Long userId,
+                                                 @RequestParam("type") UserCredential.Type type);
 }
