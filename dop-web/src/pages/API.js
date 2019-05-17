5a9efca468f266c8827457e5ae8e838acf8c83db
任贵杰 <812022339@qq.com>
2019-05-17 12:31:42 +0800

    fix host

--- dop-web/src/pages/API.js
+++ dop-web/src/pages/API.js
@@ -1,7 +1,7 @@
 // 配置所有接口的API文件
 
-// const host = "http://open.dop.clsaa.com";
-const host = "http://localhost:8888";
+const host = "http://open.dop.clsaa.com";
+// const host = "http://localhost:8888";
 const pipeline = "/pipeline-server";
 const application = "/application-server";
 const permission = "/permission-server";
@@ -20,7 +20,6 @@ const API = {
     user: host + user,
     image: host + image,
     test: host + test
-    // test: 'http://localhost:13700'
 };
 
 export default API;
