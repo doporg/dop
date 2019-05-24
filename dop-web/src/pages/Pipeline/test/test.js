0a05ea12741c6f39cf1c9f6faaba713c3958dca4
zhangfuli <1171067930@qq.com>
2019-05-24 12:21:41 +0800

    分页

--- dop-web/src/pages/Pipeline/test/test.js
+++ dop-web/src/pages/Pipeline/test/test.js
@@ -1,32 +1,39 @@
 import React, {Component} from 'react';
-import {injectIntl, FormattedMessage} from 'react-intl';
-import Shell from '../components/chosenSteps/Shell'
-import Axios from 'axios'
-import API from '../../API'
+import {injectIntl} from 'react-intl';
+import 'antd/dist/antd.css';
+import { Table } from 'antd';
 
-
+const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        width: 150,
+    },
+    {
+        title: 'Age',
+        dataIndex: 'age',
+        width: 150,
+    },
+    {
+        title: 'Address',
+        dataIndex: 'address',
+    },
+];
+const data = [];
+for (let i = 0; i < 100; i++) {
+    data.push({
+        key: i,
+        name: `Edward King ${i}`,
+        age: 32,
+        address: `London, Park Lane no. ${i}`,
+    });
+}
 class PipelineTest extends Component {
-    componentWillMount() {
-        Axios.post("http://localhost:13600/v1/resultOutput?id=5cda9322d65aed0001d807e0").then()
-        Axios({
-            url: "http://localhost:13600/v1/resultOutput/notify/5cda9322d65aed0001d807e0",
-            method: "post",
-            data: null,
-            headers: {
-                "x-login-user": 39
-            }
-        })
-    }
 
     render() {
         return (
             <div>
-                {this.props.intl.messages["pipeline.hello"]}
-                <FormattedMessage
-                    id="pipeline.hello"
-                    defaultMessage="你好"
-                />
-                <Shell/>
+                <Table columns={columns} dataSource={data} pagination={{ pageSize: 50 }} />,
             </div>
         )
     }
