13eba46d71e22e78cbd0ba3ac66571bc91fd93ff
151250010 <851642452@qq.com>
2019-06-12 19:18:05 +0800

    fix bug

--- test-server/src/main/java/com/clsaa/dop/server/test/model/param/RequestParamCreateParam.java
+++ test-server/src/main/java/com/clsaa/dop/server/test/model/param/RequestParamCreateParam.java
@@ -1,8 +1,10 @@
 package com.clsaa.dop.server.test.model.param;
 
 import com.clsaa.dop.server.test.enums.ParamClass;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
@@ -13,6 +15,8 @@ import javax.validation.constraints.NotNull;
  */
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RequestParamCreateParam {
 
     private ParamClass paramClass;
