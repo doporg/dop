f73d855643cc9551ab640db10f62e480dd6f72df
Jerry1ee <34805730+552000264@users.noreply.github.com>
2020-02-27 12:59:08 +0800

    Done:     修改数据库新建表规则，引擎改为InnoDB
    Todo:
        。。。

--- pipeline-server/src/main/java/com/clsaa/dop/server/pipeline/config/DruidDataSourceConfig.java
+++ pipeline-server/src/main/java/com/clsaa/dop/server/pipeline/config/DruidDataSourceConfig.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -21,6 +22,7 @@ import javax.sql.DataSource;
 @Setter
 @SpringBootConfiguration
 @EnableTransactionManagement
+@Component
 @ConfigurationProperties(prefix = "spring.datasource")
 public class DruidDataSourceConfig {
 
