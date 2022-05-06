b5fc88669d498af353c7627e8007383e88f30851
TangliziGit <tanglizimail@foxmail.com>
2022-05-06 17:05:15 +0800

    chore: add pom dependencies to enable eureka
    Signed-off-by: TangliziGit <tanglizimail@foxmail.com>

--- defect-location-server/backend/src/main/java/cn/com/devopsplus/dop/server/defect/DefectLocationApplication.java
+++ defect-location-server/backend/src/main/java/cn/com/devopsplus/dop/server/defect/DefectLocationApplication.java
@@ -4,10 +4,12 @@ import com.github.jeffreyning.mybatisplus.conf.EnableMPP;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan("cn.com.devopsplus.dop.server.defect.mapper")
 @EnableMPP
+@EnableEurekaClient
 public class DefectLocationApplication {
 
 	public static void main(String[] args) {
